a8f910faaceda9a57c789d979767a9e48f65e7c2
Brunno Santana <brunnosantana98@gmail.com>
2024-03-14 19:20:48 -0300

    testes do validador de estado pertence ao país

---
+++ src/test/java/br/com/brunno/bookstore/paymentFlow/validator/StateBelongToCountryValidatorTest.java
@@ -0,0 +1,95 @@
+package br.com.brunno.bookstore.paymentFlow.validator;
+
+import br.com.brunno.bookstore.country.Country;
+import br.com.brunno.bookstore.paymentflow.NewPurchaseRequest;
+import br.com.brunno.bookstore.paymentflow.validator.StateBelongToCountryValidator;
+import br.com.brunno.bookstore.state.State;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+import java.util.List;
+
+public class StateBelongToCountryValidatorTest {
+
+    EntityManager entityManager = Mockito.mock(EntityManager.class);
+
+    StateBelongToCountryValidator stateBelongToCountryValidator = new StateBelongToCountryValidator(entityManager);
+
+    /*
+    (a) country has state
+    (b) state belong to country
+
+    a && b  =
+    v    v  valido
+    v    f  invalido
+    f    v  valido
+    f    f  valido
+
+    - quando pais tiver estado e estado pertencer ao pais então é valido
+    - quando tiver pais mas o estado não pertencer ao pais então é invalido
+    - quando o pais não tiver estado então é valido
+     */
+
+    @Test
+    @DisplayName("quando pais tiver estado e estado pertencer ao pais então é valido")
+    void hasCountryAndStateValid() {
+        Country brazil = new Country("Brazil");
+        State rioDeJaneiro = new State("Rio de Janeiro", brazil);
+        Mockito.doReturn(brazil).when(entityManager).find(Country.class, 1L);
+        Mockito.doReturn(rioDeJaneiro).when(entityManager).find(State.class, 1L);
+
+        NewPurchaseRequest request = new NewPurchaseRequest();
+        ReflectionTestUtils.setField(request, "countryId", 1L);
+        ReflectionTestUtils.setField(request, "stateId", 1L);
+        Errors errors = new BeanPropertyBindingResult(request, "test");
+
+
+        stateBelongToCountryValidator.validate(request, errors);
+
+
+        Assertions.assertThat(errors.hasErrors()).isFalse();
+    }
+
+    @Test
+    @DisplayName("quando tiver pais mas o estado não pertencer ao pais então é invalido")
+    void stateDoesnNotBelongToCountry() {
+        Country argentina = new Country("Argentina");
+        ReflectionTestUtils.setField(argentina, "states", List.of(new State("Buenos Aires", argentina)));
+        State rioDeJaneiro = new State("Rio de Janeiro", new Country("Brazil"));
+        Mockito.doReturn(argentina).when(entityManager).find(Country.class, 1L);
+        Mockito.doReturn(rioDeJaneiro).when(entityManager).find(State.class, 1L);
+
+        NewPurchaseRequest request = new NewPurchaseRequest();
+        ReflectionTestUtils.setField(request, "countryId", 1L);
+        ReflectionTestUtils.setField(request, "stateId", 1L);
+        Errors errors = new BeanPropertyBindingResult(request, "test");
+
+
+        stateBelongToCountryValidator.validate(request, errors);
+
+
+        Assertions.assertThat(errors.hasErrors()).isTrue();
+        Assertions.assertThat(errors.getFieldError("stateId")).isNotNull();
+    }
+
+    @Test
+    @DisplayName("quando o pais não tiver estado então é valido")
+    void validWhenCountryNasState() {
+        Country brazil = new Country("Brazil");
+        Mockito.doReturn(brazil).when(entityManager).find(Country.class, 1L);
+        NewPurchaseRequest request = new NewPurchaseRequest();
+        ReflectionTestUtils.setField(request, "countryId", 1L);
+        Errors errors = new BeanPropertyBindingResult(request, "test");
+
+        stateBelongToCountryValidator.validate(request, errors);
+
+        Assertions.assertThat(errors.hasErrors()).isFalse();
+    }
+
+}
