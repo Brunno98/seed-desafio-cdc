bb59f1637662cd4de0b29ae25db356b5faf8eef2
Brunno Santana <brunnosantana98@gmail.com>
2024-04-07 21:02:16 -0300

    criacao de autor - desacoplando de banco de dados

--- src/main/java/br/com/brunno/bookstore/author/AuthorController.java
+++ src/main/java/br/com/brunno/bookstore/author/AuthorController.java
@@ -1,29 +1,23 @@
 package br.com.brunno.bookstore.author;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Clock;
-
 @RestController
 @RequestMapping("/author")
 public class AuthorController {
 
-    private final EntityManager entityManager;
+    private final AuthorRepository authorRepository;
 
     @Autowired
-    public AuthorController(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     @Transactional
@@ -31,7 +25,7 @@ public class AuthorController {
     public ResponseEntity<CreateAuthorResponse> createAuthor(@RequestBody @Valid CreateAuthorRequest createRequest) {
         Author author = createRequest.toDomain();
 
-        entityManager.persist(author);
+        authorRepository.save(author);
 
         CreateAuthorResponse response = CreateAuthorResponse.from(author) ;
 
---
+++ src/main/java/br/com/brunno/bookstore/author/AuthorRepository.java
@@ -0,0 +1,6 @@
+package br.com.brunno.bookstore.author;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
